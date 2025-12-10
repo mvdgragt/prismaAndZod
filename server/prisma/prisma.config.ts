c1adc2452cc7471220824416e0e903f5d785c452
mvdgragt <michiel.vandergragt@sundsgarden.se>
2025-12-10 19:53:37 +0100

    updating prisma.cinfig.ts for aws

--- server/prisma/prisma.config.ts
+++ server/prisma/prisma.config.ts
@@ -1,1 +1,20 @@
+// prisma.config.ts
+
+// 1. Import dotenv (CRITICAL for CLI to load .env on EC2)
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  // The schema path is required
+  schema: "prisma/schema.prisma",
+
+  // 2. The datasource property is now required for the CLI
+  datasource: {
+    // 3. Use the DIRECT_DATABASE_URL for migrations (TCP connection)
+    url: env("DIRECT_DATABASE_URL"),
+  },
+  // You can also add your migrations path here
+  migrations: {
+    path: "prisma/migrations",
+  },
+});
